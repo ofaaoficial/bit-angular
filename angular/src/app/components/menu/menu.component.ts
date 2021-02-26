520f5c356a00d6740e1a1dd78174bf9b15ca2da9
Oscar Amado <oscarfamado@gmail.com>
2021-02-26 10:38:07 -0500

    menu implementation

--- angular/src/app/components/menu/menu.component.ts
+++ angular/src/app/components/menu/menu.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from 'src/app/services/auth.service';
+import firebase from 'firebase';
+import User = firebase.User;
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  iconDetails: object[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
   }
 
+  get user(): User {
+    return this.authService.user;
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  ngOnInit(): void {
+    this.iconDetails = [
+      {
+        label: 'Login',
+        source: 'assets/icons/user.png',
+        link: 'login',
+      },
+      {
+        label: 'Logout',
+        source: 'assets/icons/salir.svg'
+      }
+    ];
+  }
+
+  async logout(): Promise<any> {
+    console.log('click logout');
+    this.authService.logout().then(() => {
+      this.authService.setUser(undefined);
+      this.router.navigate(['/']);
+    });
+  }
+
+  alert(label: string): void {
+    console.log('You clicked ', label);
+  }
 }
